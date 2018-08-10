55a67675b539805e79611d6cb1a0815613e86c03
Gabriel Packer <ggkpacker@gmail.com>
2018-08-10 08:42:32 -0300

    My identity for @lewagon in the gitconfig

--- install.sh
+++ install.sh
@@ -39,11 +39,11 @@ cd "$CURRENT_DIR"
 
 setopt nocasematch
 if [[ ! `uname` =~ "darwin" ]]; then
-  git config --global core.editor "subl -n -w $@ >/dev/null 2>&1"
-  echo 'export BUNDLER_EDITOR="subl $@ >/dev/null 2>&1 -a"' >> zshrc
+  git config --global core.editor "subl3 -n -w $@ >/dev/null 2>&1"
+  echo 'export BUNDLER_EDITOR="subl3 $@ >/dev/null 2>&1 -a"' >> zshrc
 else
-  git config --global core.editor "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' -n -w"
-  bundler_editor="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
+  git config --global core.editor "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl3' -n -w"
+  bundler_editor="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl3'"
   echo "export BUNDLER_EDITOR=\"${bundler_editor} -a\"" >> zshrc
 fi
 
